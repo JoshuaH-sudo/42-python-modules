ddfe4406060a9706879301c51abb38c8a4481d1e
Joshua-Hoban-visi-one <j.hoban@visi-one.com>
2026-02-07 08:51:46 +1000

    Enhance ft_command_quest function to display received argument count and improve output formatting; add new ft_score_analytics module

--- module-03/ex0/ft_command_quest.py
+++ module-03/ex0/ft_command_quest.py
@@ -5,12 +5,16 @@ def ft_command_quest() -> None:
     print("=== Command Quest ===")
     program_name = sys.argv[0]
     total_arguments = len(sys.argv)
+    received_arguments = len(sys.argv[1:])
+
     if len(sys.argv) < 2:
         print("No arguments provided!")
     print(f"Program name: {program_name}")
+
+    if received_arguments > 1:
+        print(f"Arguments received: {received_arguments}")
+
     i = 1
-    if total_arguments > 1:
-        print(f"Arguments received: {total_arguments - 1}")
     while i < total_arguments:
         print(f"Argument {i}: {sys.argv[i]}")
         i += 1
