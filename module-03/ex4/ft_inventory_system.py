d1efa528dd39fcf97063a903c1bf343f8e98284f
Joshua-Hoban-visi-one <j.hoban@visi-one.com>
2026-02-09 16:33:03 +1000

    Refactor Inventory class to InventoryManagement, enhance item handling, and improve inventory statistics and management suggestions

--- module-03/ex4/ft_inventory_system.py
+++ module-03/ex4/ft_inventory_system.py
@@ -2,24 +2,24 @@ def pluralize(unit: int, singular: str, plural: str) -> str:
     return singular if unit == 1 else plural
 
 
-class Inventory:
-    items = dict()
+class InventoryManagement:
+    inventory: dict[str, int] = dict()
 
-    def __init__(self, items: dict) -> None:
-        self.items = items
+    def __init__(self, items: dict[str, int]) -> None:
+        self.inventory = items
 
     def get_unique_items(self) -> set[str]:
-        return set(self.items.keys())
+        return set(self.inventory.keys())
 
     def get_total_items(self) -> int:
         total = 0
-        for quantity in self.items.values():
+        for quantity in self.inventory.values():
             total += quantity
         return total
 
     def print_inventory(self) -> None:
         print("=== Current Inventory ===")
-        for item, quantity in self.items.items():
+        for item, quantity in self.inventory.items():
             percentage = (quantity / self.get_total_items()) * 100
             print(
                 f"{item}: {quantity}",
@@ -30,22 +30,28 @@ class Inventory:
     def inventory_statistics(self) -> None:
         most_abundant: dict[str, int] | None = None
         least_abundant: dict[str, int] | None = None
-        for item, quantity in self.items.items():
-            if most_abundant is None or quantity > self.items[most_abundant]:
+        for item, quantity in self.inventory.items():
+            if (
+                most_abundant is None
+                or quantity > self.inventory[most_abundant]
+            ):
                 most_abundant = item
-            if least_abundant is None or quantity < self.items[least_abundant]:
+            if (
+                least_abundant is None
+                or quantity < self.inventory[least_abundant]
+            ):
                 least_abundant = item
 
         print("=== Inventory Statistics ===")
         print(
             f"Most abundant: {most_abundant}",
-            f"({self.items[most_abundant]}",
-            f"{pluralize(self.items[most_abundant], 'unit', 'units')})",
+            f"({self.inventory[most_abundant]}",
+            f"{pluralize(self.inventory[most_abundant], 'unit', 'units')})",
         )
         print(
             f"Least abundant: {least_abundant}",
-            f"({self.items[least_abundant]}",
-            f"{pluralize(self.items[least_abundant], 'unit', 'units')})",
+            f"({self.inventory[least_abundant]}",
+            f"{pluralize(self.inventory[least_abundant], 'unit', 'units')})",
         )
         print()
 
@@ -54,7 +60,7 @@ class Inventory:
         scarce_items = {}
 
         # if units is greater than 5, it's moderate, otherwise it's scarce
-        for item, quantity in self.items.items():
+        for item, quantity in self.inventory.items():
             if quantity >= 5:
                 moderate_items[item] = quantity
             else:
@@ -68,7 +74,7 @@ class Inventory:
     def management_suggestions(self) -> None:
         # if units equals to 1, suggest to restock
         restock_suggestions = []
-        for item, quantity in self.items.items():
+        for item, quantity in self.inventory.items():
             if quantity == 1:
                 restock_suggestions.append(item)
         print("=== Management Suggestions ===")
@@ -77,24 +83,25 @@ class Inventory:
 
 
 def ft_inventory_system() -> None:
-    inventory = Inventory(
+    inventoryManagement = InventoryManagement(
         {"potion": 5, "armor": 3, "shield": 2, "sword": 1, "helmet": 1}
     )
     print("=== Inventory System Analysis ===")
-    print(f"Total items in inventory: {inventory.get_total_items()}")
-    print(f"Unique items types: {inventory.get_unique_items()}")
+    print(f"Total items in inventory: {inventoryManagement.get_total_items()}")
+    print(f"Unique items types: {inventoryManagement.get_unique_items()}")
     print()
 
-    inventory.print_inventory()
-    inventory.inventory_statistics()
-    inventory.print_categories()
-    inventory.management_suggestions()
+    inventoryManagement.print_inventory()
+    inventoryManagement.inventory_statistics()
+    inventoryManagement.print_categories()
+    inventoryManagement.management_suggestions()
 
     print("=== Dictionary Properties Demo ===")
-    print(f"Dictionary keys: {inventory.items.keys()}")
-    print(f"Dictionary values: {inventory.items.values()}")
+    print(f"Dictionary keys: {inventoryManagement.inventory.keys()}")
+    print(f"Dictionary values: {inventoryManagement.inventory.values()}")
     print(
-        f"Sample lookup - 'sword' in inventory: {'sword' in inventory.items}"
+        "Sample lookup - 'sword' in inventory:",
+        f"{'sword' in inventoryManagement.inventory}",
     )
 
 
