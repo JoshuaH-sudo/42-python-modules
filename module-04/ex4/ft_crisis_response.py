f64591f818dbd6a8cf3804d528d8acfd81f27ea2
jhoban <jhoban@student.42berlin.de>
2026-03-14 12:59:04 +0100

    Refactor ft_crisis_response function for improved error handling and clarity in crisis scenarios

--- module-04/ex4/ft_crisis_response.py
+++ module-04/ex4/ft_crisis_response.py
@@ -4,28 +4,43 @@ def ft_crisis_response() -> None:
 	lost_file_name = 'lost_archive.txt'
 	forbidden_file_name = 'classified_vault.txt'
 	standard_file_name = 'standard_archive.txt'
-	print("Initiating crisis response protocols...")
-	try:
-		with open(read_file_name, 'r') as file:
-			print("Crisis data accessed successfully.\n")
 
-			print("CRISIS ANALYSIS:")
+	# Access file that does not exist
+	print(f"CRISIS ALERT: Attemping access to '{lost_file_name}'...")
+	try:
+		with open(lost_file_name, 'r') as file:
+			print("Access granted to lost archive. Retrieving data...\n")
 			for line in file:
 				print(line.strip())
 	except IOError:
-		print(f"Error: Unable to access {read_file_name}. Crisis response compromised.")
-	print()
+		print("RESPONSE: Archive not found in storage martix")
+	finally:
+		print("STATUS: Crisis handled, system stable")
 
+	# Access file without permissions
+	print(f"\nCRISIS ALERT: Attempting access to '{forbidden_file_name}'...")
 	try:
-		with open(write_file_name, 'w') as file:
-			print("CRISIS RESPONSE LOGGING:")
-			new_line = "[CRISIS RESPONSE] New crisis response protocols logged\n"
-			print(new_line)
-			file.write(new_line)
-			print("Crisis response protocols logged successfully")
+		with open(forbidden_file_name, 'r') as file:
+			print("Access granted to forbidden archive. Retrieving data...\n")
+			for line in file:
+				print(line.strip())
 	except IOError:
-		print(f"Error: Unable to write to {write_file_name}. Crisis response compromised.")
-	print("All crisis response operations completed with maximum efficiency.")
+		print("RESPONSE: Secuirty protocols deny access")
+	finally:
+		print("STATUS: Crisis handled, system maintained")
 
+	# Access file with permissions
+	print(f"\nCRISIS ALERT: Attempting access to '{standard_file_name}'...")
+	try:
+		with open(standard_file_name, 'r') as file:
+			print("Archived recovered - ", end="")
+			for line in file:
+				print(f"''{line.strip()}''")
+	except IOError:		
+		print("RESPONSE: Unexpected error accessing standard archive")
+	finally:
+		print("STATUS: Normal operations resumed")
+
+	print("\nAll crisis scenarios handled successfully. Archives secure.")
 if __name__ == "__main__":
 	ft_crisis_response()
