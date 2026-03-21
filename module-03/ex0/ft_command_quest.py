3bbd913d2dabaebfc8360c7c37a0737ddd982f9f
jhoban <jhoban@student.42berlin.de>
2026-03-21 11:06:57 +0100

    Refactor ft_command_quest to improve argument handling and clarity in output

--- module-03/ex0/ft_command_quest.py
+++ module-03/ex0/ft_command_quest.py
@@ -3,15 +3,15 @@ import sys
 
 def ft_command_quest() -> None:
     print("=== Command Quest ===")
-    program_name = sys.argv[0]
+    program_name = sys.argv[0].split("/")[-1]
     total_arguments = len(sys.argv)
     received_arguments = len(sys.argv[1:])
 
-    if len(sys.argv) < 2:
-        print("No arguments provided!")
     print(f"Program name: {program_name}")
 
-    if received_arguments > 0:
+    if received_arguments == 0:
+        print("No arguments provided!")
+    else:
         print(f"Arguments received: {received_arguments}")
 
     i = 1
