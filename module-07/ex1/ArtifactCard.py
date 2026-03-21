71ec9f13a9d420e2170e58e2f9a4cb884191e295
jhoban <jhoban@student.42berlin.de>
2026-03-21 15:33:00 +0100

    Add initial implementation of ArtifactCard, Deck, SpellCard, and main module files

---
+++ module-07/ex1/ArtifactCard.py
@@ -0,0 +1,7 @@
+from ex0 import Card
+
+
+class ArtifactCard(Card):
+    def __init__(self, name: str, cost: int, rarity: str, effect_type: str):
+        super().__init__(name, cost, rarity)
+        self.effect_type = effect_type
